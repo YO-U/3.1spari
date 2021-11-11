b27df1ed47e4fb6bf777b83598f0aab073f38231
enoxy <enoxy@DESKTOP-8BHQKRR>
2021-11-11 13:07:54 +0200

    Добавьте файлы проекта.

---
+++ 3.1spari/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _3._1spari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите n:");
+                int n = int.Parse(Console.ReadLine());
+                double Q = 0;
+                double F = 1;
+                for (int k = 1; k <= n; k++)
+                {
+                    for (int i = 1; i <= n - k; i++) F *= i;
+                    {
+                        if ( n == k) continue;
+                        Q += Math.Pow(-1, k) * (k - 7) / 2 * F;
+
+                    }                  
+                }
+                Console.WriteLine($"Q={Q:F2}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
